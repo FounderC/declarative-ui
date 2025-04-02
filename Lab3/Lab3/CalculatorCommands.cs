f293ce370987ddf01049584afff717b236e74569
Ваше Ім’я <khomenko.oleg@hotmail.com>
2025-04-02 04:50:52 +0300

    Add Lab3

---
+++ Lab3/Lab3/CalculatorCommands.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Input;
+
+namespace Lab3
+{
+    public static class CalculatorCommands
+    {
+        // Цифри 0-9
+        public static readonly RoutedUICommand Num0 =
+            new RoutedUICommand("Num0", "Num0", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num1 =
+            new RoutedUICommand("Num1", "Num1", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num2 =
+            new RoutedUICommand("Num2", "Num2", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num3 =
+            new RoutedUICommand("Num3", "Num3", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num4 =
+            new RoutedUICommand("Num4", "Num4", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num5 =
+            new RoutedUICommand("Num5", "Num5", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num6 =
+            new RoutedUICommand("Num6", "Num6", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num7 =
+            new RoutedUICommand("Num7", "Num7", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num8 =
+            new RoutedUICommand("Num8", "Num8", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Num9 =
+            new RoutedUICommand("Num9", "Num9", typeof(CalculatorCommands));
+
+        // Крапка (.)
+        public static readonly RoutedUICommand Dot =
+            new RoutedUICommand("Dot", "Dot", typeof(CalculatorCommands));
+
+        // Операції
+        public static readonly RoutedUICommand Add =
+            new RoutedUICommand("Add", "Add", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Subtract =
+            new RoutedUICommand("Subtract", "Subtract", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Multiply =
+            new RoutedUICommand("Multiply", "Multiply", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Divide =
+            new RoutedUICommand("Divide", "Divide", typeof(CalculatorCommands));
+
+        public static readonly RoutedUICommand Clear =
+            new RoutedUICommand("Clear", "Clear", typeof(CalculatorCommands));
+        public static readonly RoutedUICommand Calculate =
+            new RoutedUICommand("Calculate", "Calculate", typeof(CalculatorCommands));
+    }
+}
---
+++ Lab3/Lab3/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using Lab3;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab3
+{
+    public partial class MainWindow : Window
+    {
+        // Змінні для зберігання першого операнда, операції тощо
+        private double? _firstValue = null;
+        private string _operation = null;
+        private bool _isNewEntry = true; // коли true, ми набираємо нове число (очищуємо Display)
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Створюємо CommandBinding для кожної команди
+            // (цифри)
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num0, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num1, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num2, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num3, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num4, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num5, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num6, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num7, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num8, Execute_Num, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Num9, Execute_Num, CanExecute_Always));
+
+            // Крапка
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Dot, Execute_Dot, CanExecute_Always));
+
+            // Операції
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Add, Execute_Operation, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Subtract, Execute_Operation, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Multiply, Execute_Operation, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Divide, Execute_Operation, CanExecute_Always));
+
+            // Clear і Calculate
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Clear, Execute_Clear, CanExecute_Always));
+            CommandBindings.Add(new CommandBinding(CalculatorCommands.Calculate, Execute_Calculate, CanExecute_Always));
+
+            // Початковий текст
+            txtDisplay.Text = "0";
+        }
+
+        // ---- Цифри ----
+        private void Execute_Num(object sender, ExecutedRoutedEventArgs e)
+        {
+            string num = null;
+            if (e.Command == CalculatorCommands.Num0) num = "0";
+            else if (e.Command == CalculatorCommands.Num1) num = "1";
+            else if (e.Command == CalculatorCommands.Num2) num = "2";
+            else if (e.Command == CalculatorCommands.Num3) num = "3";
+            else if (e.Command == CalculatorCommands.Num4) num = "4";
+            else if (e.Command == CalculatorCommands.Num5) num = "5";
+            else if (e.Command == CalculatorCommands.Num6) num = "6";
+            else if (e.Command == CalculatorCommands.Num7) num = "7";
+            else if (e.Command == CalculatorCommands.Num8) num = "8";
+            else if (e.Command == CalculatorCommands.Num9) num = "9";
+
+            if (num == null) return;
+
+            if (_isNewEntry)
+            {
+                // Якщо ми щойно натиснули операцію або =
+                txtDisplay.Text = num;
+                _isNewEntry = false;
+            }
+            else
+            {
+                // Дописуємо до кінця
+                if (txtDisplay.Text == "0")
+                    txtDisplay.Text = num;
+                else
+                    txtDisplay.Text += num;
+            }
+        }
+
+        // ---- Додаємо крапку (.) ----
+        private void Execute_Dot(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_isNewEntry)
+            {
+                // Починаємо нове число з "0."
+                txtDisplay.Text = "0.";
+                _isNewEntry = false;
+            }
+            else
+            {
+                if (!txtDisplay.Text.Contains("."))
+                    txtDisplay.Text += ".";
+            }
+        }
+
+        // ---- Операції (+, -, ×, ÷) ----
+        private void Execute_Operation(object sender, ExecutedRoutedEventArgs e)
+        {
+            double value;
+            if (double.TryParse(txtDisplay.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out value))
+            {
+                if (_firstValue.HasValue && _operation != null && !_isNewEntry)
+                {
+                    // Виконуємо проміжний розрахунок
+                    _firstValue = CalculateResult(_firstValue.Value, value, _operation);
+                    txtDisplay.Text = _firstValue.Value.ToString(CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    _firstValue = value;
+                }
+            }
+
+            // Задаємо операцію
+            if (e.Command == CalculatorCommands.Add) _operation = "+";
+            else if (e.Command == CalculatorCommands.Subtract) _operation = "-";
+            else if (e.Command == CalculatorCommands.Multiply) _operation = "*";
+            else if (e.Command == CalculatorCommands.Divide) _operation = "/";
+
+            _isNewEntry = true;
+        }
+
+        // ---- Кнопка "=" ----
+        private void Execute_Calculate(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_firstValue.HasValue && _operation != null && !_isNewEntry)
+            {
+                double secondValue;
+                if (double.TryParse(txtDisplay.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out secondValue))
+                {
+                    double? result = CalculateResult(_firstValue.Value, secondValue, _operation);
+                    if (result.HasValue)
+                    {
+                        txtDisplay.Text = result.Value.ToString(CultureInfo.InvariantCulture);
+                        _firstValue = result.Value;
+                    }
+                }
+            }
+            // Після натискання "=" готуємося до нового введення
+            _operation = null;
+            _isNewEntry = true;
+        }
+
+        // ---- "C" (Clear) ----
+        private void Execute_Clear(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtDisplay.Text = "0";
+            _firstValue = null;
+            _operation = null;
+            _isNewEntry = true;
+        }
+
+        // Допоміжний метод для обчислень
+        private double? CalculateResult(double first, double second, string op)
+        {
+            switch (op)
+            {
+                case "+": return first + second;
+                case "-": return first - second;
+                case "*": return first * second;
+                case "/":
+                    if (second == 0)
+                    {
+                        MessageBox.Show("Ділення на нуль неможливе!");
+                        return null;
+                    }
+                    return first / second;
+                default: return second;
+            }
+        }
+
+        // CanExecute - завжди true (спрощено)
+        private void CanExecute_Always(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
