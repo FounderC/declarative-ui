ccbf88b8bdf179a55cdef0d9c7cd1ef843af4438
Ваше Ім’я <khomenko.oleg@hotmail.com>
2025-04-15 20:41:49 +0300

    Ex 2

---
+++ Лаб4/Лаб4/AdoAssistant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+
+namespace Лаб4
+{
+    public class AdoAssistant
+    {
+        // Отримання рядка підключення з App.config (переконайтеся, що ім'я відповідає)
+        private string connectionString = ConfigurationManager.ConnectionStrings["connectionString_ADO"].ConnectionString;
+
+        // Змінна для зберігання даних (DataTable)
+        private DataTable dt = null;
+
+        // Метод, що завантажує дані з БД у DataTable
+        public DataTable TableLoad()
+        {
+            if (dt != null)
+                return dt;
+
+            dt = new DataTable();
+
+            // Використовуємо using для автоматичного закриття підключення
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Створення команди
+                SqlCommand command = connection.CreateCommand();
+                // Наприклад, вибірка даних із таблиці Table_Address
+                command.CommandText = "SELECT ID, (Surname + ' ' + Name) AS FullName, Address, Place, PostalCode FROM Table_Address";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                try
+                {
+                    // Відкриття підключення та завантаження даних
+                    connection.Open();
+                    adapter.Fill(dt);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка підключення до БД: " + ex.Message);
+                }
+            }
+
+            return dt;
+        }
+    }
+}
--- Лаб4/Лаб4/MainWindow.xaml.cs
+++ Лаб4/Лаб4/MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Лаб4;
 
 namespace Лаб4
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();  // Викликаємо метод завантаження даних при запуску вікна
+        }
+
+        private void LoadData()
+        {
+            // Створення екземпляру класу AdoAssistant
+            AdoAssistant ado = new AdoAssistant();
+
+            // Отримання DataTable з даними
+            DataTable dt = ado.TableLoad();
+
+            // Прив'язка даних до DataGrid. Використовується DefaultView, щоб DataGrid міг коректно їх відобразити
+            dataGrid.ItemsSource = dt.DefaultView;
         }
     }
 }
